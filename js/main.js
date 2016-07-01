492615f080d633e04786a7190ab9ee9b453fd114
David Ibáñez <dibanez@gmail.com>
2016-07-01 15:05:31 +0200

    agregando secciones

--- js/main.js
+++ js/main.js
@@ -187,6 +187,64 @@
 
 
 	/*----------------------------------------------------*/
+	/*	gmaps settings
+	------------------------------------------------------*/
+
+	   var map;
+
+	   // main directions
+	   map = new GMaps({
+	      el: '#map',
+	      lat: 42.8708009,
+	      lng: -8.5258937,
+	      zoom: 15,
+	      zoomControl : true,
+	      zoomControlOpt: { style : 'SMALL', position: 'TOP_LEFT' },
+	      panControl : false,
+	      scrollwheel: false
+	   });
+
+	   map.drawOverlay({
+		   lat: map.getCenter().lat(),
+		   lng: map.getCenter().lng(),
+		   content: '<i class="fa fa-map-marker"></i>',
+		   verticalAlign: 'top',
+		   horizontalAlign: 'center'
+		});
+
+	   // The styles below present a simplified map.
+	   // If you would like to use a normal coloured map, then please remove or comment the code below, from lines 128 to 148.
+		var mapStyles = [
+		   {
+		      featureType: "road",
+		      elementType: "geometry",
+		      stylers: [{
+		         lightness: 100
+		      }, {
+		         visibility: "simplified"
+		      }]
+		   }, {
+		      featureType: "road",
+		      elementType: "labels",
+		      stylers: [{
+		         visibility: "off"
+		      }]
+		   }
+		];
+
+		map.setOptions({
+		   styles: mapStyles
+		});
+
+    //map.addMarker({
+ 		//  lat: map.getCenter().lat(),
+	  //  lng: map.getCenter().lng(),
+ 		//  title: 'Ciudad de Cultura',
+    //	infoWindow: { content: '<p>You can add your address 1 here</p>' }
+    //});
+
+
+	/*----------------------------------------------------*/
 	/* Final Countdown Settings
 	------------------------------------------------------ */
 	var finalDate = '2016/09/17';
